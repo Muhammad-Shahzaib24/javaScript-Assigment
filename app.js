52c28c09b864f395b87a89fa4810fc48815eadb4
Muhammad_Shahzaib24 <mdshahzaib729@gmail.com>
2023-09-01 11:25:13 -0700

    work complet

---
+++ app.js
@@ -0,0 +1,84 @@
+
+//Q1
+alert("Hello world");
+
+
+// Q2
+var thks = "hello world"
+document.write(thks);
+
+
+// Q3
+alert("wellcome to \n js coding");
+
+
+// Q4
+var firstName = "Muhammad";
+var lastName = "Shahzaib";
+var passOutClass = "Enter";
+var yourSkills = "Web Developer";
+var yourFuterGoll = "billioner";
+
+ alert (`firstName:` + " " + firstName);
+ alert(lastName);
+ alert(passOutClass);
+ alert(yourFuterGoll);
+ alert(yourSkills);
+
+
+// Q5
+ var name = "hello world";
+ console.log(name);
+
+
+
+
+
+
+
+// chp2 Q1
+var name;
+name = "this is me i m codeing lover";
+alert (name);
+
+
+
+
+
+
+// chp2 Q2
+var userName = "SHAHZAIB";
+var fatherName ="AbdulRaheed";
+var concate = userName + " " + fatherName;
+alert(concate);
+
+
+
+// chp2 Q3
+ var book = "A smarter way to learn";
+ alert(`i am trying to learn from the book:`+ " " + book);
+
+
+// chp2 Q5
+var alert;
+alert(alert`plzza\nplzz\nplz\npl\np`)
+
+
+
+// chp2 Q6
+var email = "mdshahzaib729@gmail.com";
+var nme = "my Email address is";
+var sum = nme + email;
+alert(sum);
+
+
+
+// chp2 Q7
+var def = "Yah! I can write Html through JavaScript";
+document.write(def);
+
+
+// chp2 Q8
+var str = "Yah! I am certified Programer";
+alert(str);
+document.write(str);    
